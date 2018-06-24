13bc1d0c47efb249f2d2c03f323b8a503a28b81b
Gricel <gricel@Gricels-MacBook-Pro.local>
2018-06-24 16:01:26 -0400

    menu mobile listo

--- src/js/ly-scripts.js
+++ src/js/ly-scripts.js
@@ -1,37 +1,54 @@
 //SCROLL THIRDPARTY SOLUTION
-function no_scrollbody()
-{
-    $html = $('html'); 
-    $body = $('body'); 
-    var initWidth = $body.outerWidth();
-    var initHeight = $body.outerHeight();
+function no_scrollbody() {
+  $html = $('html');
+  $body = $('body');
+  var initWidth = $body.outerWidth();
+  var initHeight = $body.outerHeight();
 
-    var scrollPosition = [
-        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-    ];
-    $html.data('scroll-position', scrollPosition);
-    $html.data('previous-overflow', $html.css('overflow'));
-    $html.css('overflow', 'hidden');
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);   
+  var scrollPosition = [
+    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+    self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  ];
+  $html.data('scroll-position', scrollPosition);
+  $html.data('previous-overflow', $html.css('overflow'));
+  $html.css('overflow', 'hidden');
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
 
-    var marginR = $body.outerWidth()-initWidth;
-    var marginB = $body.outerHeight()-initHeight; 
-    $body.css({'margin-right': marginR,'margin-bottom': marginB});
-}
-function scrollbody()
-{
-    $html = $('html');
-    $body = $('body');
-    $html.css('overflow', $html.data('previous-overflow'));
-    var scrollPosition = $html.data('scroll-position');
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);    
-
-    $body.css({'margin-right': 0, 'margin-bottom': 0});
-        $("body, html").css({
-                        "overflow-x":'hidden'
-        });
+  var marginR = $body.outerWidth() - initWidth;
+  var marginB = $body.outerHeight() - initHeight;
+  $body.css({
+    'margin-right': marginR,
+    'margin-bottom': marginB
+  });
 }
 
-$(document).ready(function(){
+function scrollbody() {
+  $html = $('html');
+  $body = $('body');
+  $html.css('overflow', $html.data('previous-overflow'));
+  var scrollPosition = $html.data('scroll-position');
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+  $body.css({
+    'margin-right': 0,
+    'margin-bottom': 0
+  });
+  $("body, html").css({
+    "overflow-x": 'hidden'
+  });
+}
+
+$(document).ready(function () {
+  $('button.ly-button-mob').click(function(){
+    console.log('entre');
+    if ($('nav.ly-main-nav .flex').hasClass('active')) {
+      $('nav.ly-main-nav .flex').removeClass('active');
+    }
+    else {
+      $('nav.ly-main-nav .flex').addClass('active');
+    }
+  });
+  $('nav.ly-main-nav .flex a').click(function(){
+    $('nav.ly-main-nav .flex').removeClass('active');
+  });
 });
